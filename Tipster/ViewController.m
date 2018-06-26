346de93102576991bc5ac068650d3ab5f4c352f1
Halima Monds <hmonds@fb.com>
2018-06-26 16:17:47 -0700

    Files that I edited.

--- Tipster/ViewController.m
+++ Tipster/ViewController.m
@@ -9,6 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
 
 @end
 
@@ -23,6 +28,48 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)onTap:(id)sender {
+    NSLog(@"Hello");
+    [self.view endEditing:YES];
+}
+
+
+- (IBAction)onEdit:(id)sender {
+    
+    double bill =[self.billField.text doubleValue];
+    
+    NSArray *percentages = @[@(.10), @(0.2), @(0.30)];
+    
+    double tipPercentage = [percentages[self.tipControl.selectedSegmentIndex]doubleValue];
+    double tip = tipPercentage * bill;
+    
+    double total = bill + tip;
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f",tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f",total];
+   
+}
+- (IBAction)onEditingBegin:(id)sender {
+    [UIView animateWithDuration:0.2
+                     animations:^{
+    self.billField.frame = CGRectMake(self.billField.frame.origin.x, self.billField.frame.origin.y + 30, self.billField.frame.size.width, self.billField.frame.size.height);
+        self.tipLabel.alpha = 0;
+                     }];
+    
+}
+- (IBAction)OnEditingEnd:(id)sender {
+    CGRect newFrame = self.billField.frame;
+    newFrame.origin.y -= 30;
+    
+    [UIView animateWithDuration:0.2 animations:^{
+        self.billField.frame = newFrame;
+        self.tipLabel.alpha = 1;
+    }];
+
+    
 }
 
 
